e97f8cb2883167295f87037c6b72204c45a0f567
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-02-22 23:44:40 +0700

    remove /n, height in reduce

--- histogram/histogram.js
+++ histogram/histogram.js
@@ -1,36 +1,30 @@
 const fs = require("fs");
 
 fs.readFile("input.txt", "utf-8", (err, data) => {
+inputData = data.toString().split("\n").join("").split(" ").join("").split("");
 
-inputData = data.toString().split("/n").join("").split("");
-
+let height = 0;
 const dictionary = inputData.reduce((acc, i) => {
 	if (acc.hasOwnProperty(i)) {
 		acc[i] += 1;
+
 	} else {
 		acc[i] = 1;
-	}
+	}        
+    if (height < acc[i]) {
+        height = acc[i];
+    }
 	return acc;
 }, {});
 
 let position = Object.keys(dictionary).sort((a,b) => a.charCodeAt(0)-b.charCodeAt(0));
 
-const frequently = Object.values(dictionary);
-
-let height = frequently[0];
-for (let i=0; i < frequently.length - 1; i++) {
-	if (height < frequently[i]) {
-		height = frequently[i];
-    
-	}
-}
-
 let result = '';
 for (let i = height; i > 0; i--) {
 	let row = '';
-	for (let j = 0; j < position.length - 1; j++) {
+	for (let j = 0; j < position.length; j++) {
 		if (i > dictionary[position[j]]) {
-			row += '/n';
+			row += ' ';
 		} else {
 			row += '#';
 		}
@@ -42,6 +36,6 @@ for (let i = height; i > 0; i--) {
 
 result += position.join('');
 
-fs.writeFile("output.txt", result, (err) => {})
+fs.writeFile("output.txt", result += '\n', (err) => {})
 
 });
